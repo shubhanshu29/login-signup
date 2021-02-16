17d8d88204358c7e0abfd1913dfad60466ef1084
Shubhanshu29 <shubhanshushukla6@gmail.com>
2021-02-16 16:03:24 +0530

    State variable/handler modified

--- login.js
+++ login.js
@@ -1,109 +1,107 @@
-import React, { Component} from "react";
-import { Button, Text, View,  TextInput, StyleSheet} from "react-native";
+import React, { Component } from "react";
+import { Button, Text, View, TextInput, StyleSheet } from "react-native";
 
 
-class Cat extends Component {  
-  
-  constructor(props) {
-    super(props);
+class Cat extends Component {
+
+  constructor() {
+    super();
     this.state = {
-      email: '',
-      password: ''
-    }
-    
-    this.inputFields= [
-      {
-        name: "email",
-        placeholder: "Email"
+      email: {
+        value: '',
+        placeholder: 'Email'
       },
-      {
-        name:"password",
-        placeholder: "Password"
+      password: {
+        value: '',
+        placeholder: 'Password'
       }
-    ]
+    }
   }
-  
-  render(props) {    
-    
-    return (
-      <View style = { styles.container }>
 
-        <Text style={{fontSize:40, color:"white"}}>Welcome back!</Text>
-        <Text style={{fontSize:15, color:"grey"}}>Login with your email to start talking{"\n\n\n"}</Text> 
+  handleChange(key, text) {
+    this.setState({
+      [key]: { ...this.state[key], value: text }
+    })
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+
+        <Text style={{ fontSize: 40, color: "white" }}>Welcome back!</Text>
+        <Text style={{ fontSize: 15, color: "grey" }}>Login with your email to start talking{"\n\n\n"}</Text>
 
         <View style={styles.form}>
-        {
-          this.inputFields.map( (inputObj) => {
-              return(
+          {
+            Object.keys(this.state).map((key) => {
+              const inputObj = this.state[key];
+              return (
+                <View>
                   <View>
-                      <View>
-                          <TextInput 
-                              style={{height: 60, width:320}}
-                              placeholder= {inputObj.placeholder}
-                              placeholderTextColor = "grey"
-                              backgroundColor= "black"
-                              color="white"
-                              onChangeText = {(text)=>{
-                                  this.setState({ [inputObj.name] : text })
-                                  this.setState({ [inputObj.err] : "" })
-                              }}
-                          />
-                      </View>
-                      
-                      <Text>
-                          {this.state[inputObj.err]}
-                      </Text>
+                    <TextInput
+                      style={{ height: 60, width: 320 }}
+                      placeholder={inputObj.placeholder}
+                      placeholderTextColor="grey"
+                      backgroundColor="black"
+                      color="white"
+                      onChangeText={(text) => this.handleChange(key, text)}
+                      value={inputObj.value}
+                    />
                   </View>
+
+                  <Text>
+                    {inputObj.err}
+                  </Text>
+                </View>
               )
             })
-        }
-        </View>        
-        <Text style={{fontSize:15, color:"skyblue"}}>{"\n"}Forgot Password?</Text>
-        <Text style={{fontSize:15, color:"skyblue"}}>Use a Password manager?</Text>
+          }
+        </View>
+        <Text style={{ fontSize: 15, color: "skyblue" }}>{"\n"}Forgot Password?</Text>
+        <Text style={{ fontSize: 15, color: "skyblue" }}>Use a Password manager?</Text>
         <Text>{"\n"}</Text>
-        
+
         <Button
           onPress={async () => {
-
             const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            var passw=  /^[A-Za-z]\w{7,14}$/;
-
-            if(!re.test(String(this.state.email).toLowerCase())){
+            var passw = /^[A-Za-z]\w{7,14}$/;
+            if (!re.test(String(this.state.email.value).toLowerCase())) {
               alert("Invalid email");
               return false;
             }
 
-            if(!passw.test(this.state.password)){
+            if (!passw.test(this.state.password.value)) {
               alert("Invalid Password");
               return false;
             }
-            
-            try{
-              const res= await fetch('http://192.168.1.45:8000/login', {
+
+            try {
+              const res = await fetch('http://192.168.1.45:8000/login', {
                 method: 'POST',
                 headers: {
                   'Accept': 'application/json',
                   'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                  username: this.state.email,
-                  password: this.state.password
+                  username: this.state.email.value,
+                  password: this.state.password.value
                 })
               })
-              let data=await res.json();
+              let data = await res.json();
               console.log(data)
               alert(data);
             }
-            catch(error){
+            catch (error) {
               console.log(error)
               alert(error);
             }
-            
+
           }}
           title="Login"
         />
 
-        
+
       </View>
     );
   }
@@ -113,7 +111,7 @@ class Login extends Component {
   render() {
     return (
       <>
-        <Cat/>
+        <Cat />
       </>
     );
   }
@@ -121,13 +119,13 @@ class Login extends Component {
 
 const styles = StyleSheet.create(
   {
-       container:
-       {
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1,
-          backgroundColor: '#262624'
-       }
+    container:
+    {
+      justifyContent: "center",
+      alignItems: "center",
+      flex: 1,
+      backgroundColor: '#262624'
+    }
   });
 
-export default  Login;
+export default Login;
--- register.js
+++ register.js
@@ -1,74 +1,76 @@
-import React, { Component, useState} from "react";
-import { Button, Text, TextInput, StyleSheet, ScrollView , View} from "react-native";
+import React, { Component, useState } from "react";
+import { Button, Text, TextInput, StyleSheet, ScrollView, View } from "react-native";
 
 class Cat extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      email: '',
-      password: ''
-    }
-    
-    this.inputFields= [
-      {
-        name: "email",
-        placeholder: "Email"
+      email: {
+        value: '',
+        placeholder: 'Email'
       },
-      {
-        name:"password",
-        placeholder: "Password"
+      password: {
+        value: '',
+        placeholder: 'Password'
       }
-    ]
+    }
   }
-  render(props) {
+
+  handleChange(key, text) {
+    this.setState({
+      [key]: { ...this.state[key], value: text }
+    })
+  }
+
+
+  render() {
     return (
-      <ScrollView style = { styles.container }>
-        <Text style={{ color: "white", fontSize:45, textAlign:"center"}}>{"\n"}Register{"\n"}</Text>
-        <Text style={{ color: "white"}}>Account Information{"\n"}</Text>
+      <ScrollView style={styles.container}>
+        <Text style={{ color: "white", fontSize: 45, textAlign: "center" }}>{"\n"}Register{"\n"}</Text>
+        <Text style={{ color: "white" }}>Account Information{"\n"}</Text>
 
         <View style={styles.form}>
-        {
-          this.inputFields.map( (inputObj) => {
-              return(
+          {
+            Objects.key(this.state).map((key) => {
+              const inputObj = this.state[key];
+              return (
+                <View>
                   <View>
-                      <View>
-                          <TextInput 
-                              style={{height: 60, width:320}}
-                              placeholder= {inputObj.placeholder}
-                              placeholderTextColor = "grey"
-                              color="white"
-                              onChangeText = {(text)=>{
-                                  this.setState({ [inputObj.name] : text })
-                                  this.setState({ [inputObj.err] : "" })
-                              }}
-                          />
-                      </View>
-                      
-                      <Text>
-                          {this.state[inputObj.err]}
-                      </Text>
+                    <TextInput
+                      style={{ height: 60, width: 320 }}
+                      placeholder={inputObj.placeholder}
+                      placeholderTextColor="grey"
+                      color="white"
+                      onChangeText={(text) => this.handleChange(key, text)}
+                      value= {inputObj.value}
+                    />
                   </View>
+
+                  <Text>
+                    {this.state[inputObj.err]}
+                  </Text>
+                </View>
               )
             })
-        }
+          }
         </View>
         <Text>{"\n"}</Text>
         <Button
           onPress={async () => {
             const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            var passw=  /^[A-Za-z]\w{7,14}$/;
+            var passw = /^[A-Za-z]\w{7,14}$/;
 
-            if(!re.test(String(this.state.email).toLowerCase())){
+            if (!re.test(String(this.state.email.value).toLowerCase())) {
               alert("Invalid email");
               return false;
             }
 
-            if(!passw.test(this.state.password)){
+            if (!passw.test(this.state.password.value)) {
               alert("Invalid Password");
               return false;
             }
-            
-            try{
+
+            try {
               const res = await fetch('http://192.168.1.45:8000/register/', {
                 method: 'POST',
                 headers: {
@@ -76,14 +78,14 @@ class Cat extends Component {
                   'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                  username: this.state.email,
-                  password: this.state.password
+                  username: this.state.email.value,
+                  password: this.state.password.value
                 })
               })
-              let response= await res.json();
+              let response = await res.json();
               alert(response);
             }
-            catch(error){
+            catch (error) {
               alert(error);
             }
           }}
@@ -100,7 +102,7 @@ class Register extends Component {
   render() {
     return (
       <>
-        <Cat/>
+        <Cat />
       </>
     );
   }
@@ -108,11 +110,11 @@ class Register extends Component {
 
 const styles = StyleSheet.create(
   {
-       container:
-       {
-          flex: 1,
-          backgroundColor: '#262624'
-       }
+    container:
+    {
+      flex: 1,
+      backgroundColor: '#262624'
+    }
   });
 
-export default  Register;
+export default Register;
